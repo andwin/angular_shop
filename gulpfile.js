21b010f534604c6ffc331c4a0bb9971f7e87cf8f
Anders Windelhed <anders@windelhed.se>
2015-08-15 14:50:26 +0200

    Added gulpfile with tasks for displaying the site

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    port: 3000,
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/**/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html:watch', function () {
+  gulp.watch(['./app/**/*.html'], ['html']);
+});
+
+gulp.task('copy-bower-components', function() {
+  gulp.src('./bower_components/**')
+    .pipe(gulp.dest('dist/bower_components'));
+});
+
+gulp.task('default', ['copy-bower-components', 'connect', 'html', 'html:watch']);
