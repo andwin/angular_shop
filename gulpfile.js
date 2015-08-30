00f8c0630d39717304adb6280317ab90cde11e43
Anders Windelhed <anders@windelhed.se>
2015-08-30 22:02:05 +0200

    Code formatting

--- gulpfile.js
+++ gulpfile.js
@@ -59,4 +59,13 @@ gulp.task('copy-assets', function() {
     .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('default', ['copy-bower-components', 'copy-assets', 'connect', 'html', 'html:watch', 'sass', 'sass:watch', 'typescript']);
+gulp.task('default', [
+  'copy-bower-components',
+  'copy-assets',
+  'connect',
+  'html',
+  'html:watch',
+  'sass',
+  'sass:watch',
+  'typescript'
+]);
